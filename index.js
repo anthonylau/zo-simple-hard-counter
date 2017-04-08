19eac91cd075393d65ac38757bb686e24e56e6f2
anthonylau <anthonywaiholau@gmail.com>
2017-04-08 15:11:33 +0800

    add rebuild counter from db

--- index.js
+++ index.js
@@ -10,26 +10,57 @@ const db = require('./libs/db');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-const whoIdxs = [1, 2, 3]; //who is allowed to be counted
+const whoIds = [1, 2, 3]; //who is allowed to be counted
 
-let counters = {};
-whoIdxs.forEach(who => {
-    counters[who] = 0;
-});
-// TODO load from db
+let counters = new Map();
 
+function initialize() {
+    let loadCounters = whoIds.map(who => {
+        return db.query('SELECT id FROM vote WHERE who = $1 ORDER BY id', [who])
+            .then(res => {
+                return res.rows.reduce((val, row) => {
+                    val.lastId = row.id;
+                    val.count += 1;
+                    return val;
+                }, {
+                    who,
+                    lastId: null,
+                    count: 0
+                });
+            });
+    });
+
+    Promise.all(loadCounters).then(vals => {
+        counters = new Map(vals.map(v => [v.who, v]));
+        console.log(counters);
+        app.listen(3000, function () {
+            console.log('Example app listening on port 3000!')
+        });
+    }).catch(err => {
+        console.error('Error on loading counters', err);
+        process.exit(1);
+    });
+}
+// console.trace(counters);
 console.log('initialized');
 
 app.get('/result', function (req, res) {
-    res.send('Hello World! ' + JSON.stringify(counters));
+    let result = [];
+
+    counters.forEach((v, k) => {
+        result.push([v, k]);
+    });
+
+    res.send('result' + JSON.stringify(result));
 });
 
 app.post('/vote', function (req, res) {
     const who = req.param('who');
-    if (whoIdxs.includes(who)) {
+    if (whoIds.includes(who)) {
         db.query('INSERT INTO vote (who, at) VALUES ($1, $2)', [who, new Date()])
             .then(res => {
-                counters[who] = counters[who] + 1;
+                let counter = counters.get(who);
+                counter.count += 1;
             }).catch(err => console.error('Error inserting vote', err));
 
         res.sendStatus(200);
@@ -38,6 +69,4 @@ app.post('/vote', function (req, res) {
     }
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-});
+initialize();
