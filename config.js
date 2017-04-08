9de4f5ef2e0658904668cf66092459bda450531b
anthonylau <anthonywaiholau@gmail.com>
2017-04-09 00:51:47 +0800

    extract db config to env vars

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    postgres_host: process.env.POSTGRES_HOST,
+    postgres_user: process.env.POSTGRES_USER,
+    postgres_password: process.env.POSTGRES_PASSWORD,
+    postgres_db: process.env.POSTGRES_DB
+};
